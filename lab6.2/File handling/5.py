bcd335787f5b7862aaef61a60588c4290e4eabbe
m-zhenis <156894634+m-zhenis@users.noreply.github.com>
2024-03-13 13:35:53 +0600

    Update 5.py

--- lab6.2/File handling/5.py
+++ lab6.2/File handling/5.py
@@ -1,4 +1,4 @@
-with open("C:\\Users\\arsen\\git_test\\labs\\lab 6\\Files and directories manipulation\\Example.txt", "a") as txt:
+with open("C:\\Users\\m-zhenis\\git_test\\labs\\lab6.2\\File handling\\Example.txt", "a") as txt:
     a = list(input("Write list: ").split())
     for i in a:
         txt.write(i + " ")
